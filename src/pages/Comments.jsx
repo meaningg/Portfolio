5a8c8cd3e23ff9a026ad2dee6f2d8c5ea76806a5
mngawdawd <thomassred@gmail.com>
2021-06-21 03:46:53 +0600

    v1.01
    Верстка компонента комментов.
    доработка главной страницы.

--- src/pages/Comments.jsx
+++ src/pages/Comments.jsx
@@ -1,7 +1,39 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "../sass/pages/comments.scss";
+import Fade from "react-reveal";
 function CommentsTest() {
-  return <div>test</div>;
+  const [commentInput, setCommentInput] = useState(false);
+
+  return (
+    <div className="comments__body">
+      <div className="post">
+        <div className="likebtn noselect">Like</div>
+        <div
+          onClick={
+            commentInput === false
+              ? () => {
+                  setCommentInput(true);
+                }
+              : () => {
+                  setCommentInput(false);
+                }
+          }
+          className="commentbtn noselect"
+        >
+          Comment
+        </div>
+      </div>
+      <Fade collapse mountOnEnter unmountOnExit when={commentInput}>
+        <div className="comments__box ">
+          <div className="testItem">
+            <input type="text" />
+            <button>Send</button>
+          </div>
+        </div>
+      </Fade>
+      <div className="comments">aa</div>
+    </div>
+  );
 }
 
 export default CommentsTest;
